5e12a76f7d0292f47bb2c791819ba9a2ae2b34c1
Jan Lafferton <jan@lafferton.de>
2018-05-04 08:53:33 +0200

    Log debug messages in debug mode only

--- src/de/aeschool/hashgenerator/HashGenerator.java
+++ src/de/aeschool/hashgenerator/HashGenerator.java
@@ -2,13 +2,21 @@ package de.aeschool.hashgenerator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.lang.management.ManagementFactory;
 
 public class HashGenerator {
     public static void main(String[] args) {
+        // Output logging to console
         Log.registerLogger(new ConsoleLogger());
+
+        // Check if in debug mode
+        if (ManagementFactory.getRuntimeMXBean().getInputArguments().toString().contains("jdwp")) {
+            Log.enableDebugging();
+        }
 
         Log.d("Starting application");
 
+        // Set look and feel
         try {
             Log.d("Setting system default look and feel...");
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -16,6 +24,7 @@ public class HashGenerator {
             Log.e("Setting look and feel failed... Continuing with default");
         }
 
+        // Start application
         EventQueue.invokeLater(() -> {
             MainWindow window = new MainWindow("HashGenerator");
             window.setVisible(true);
--- src/de/aeschool/hashgenerator/Log.java
+++ src/de/aeschool/hashgenerator/Log.java
@@ -1,16 +1,22 @@
 package de.aeschool.hashgenerator;
 
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Log {
     private static List<Logger> loggers = new ArrayList<>();
+    private static boolean debugging = false;
 
     public static void registerLogger(Logger logger) {
         synchronized (loggers) {
             loggers.add(logger);
         }
+    }
+
+    public static void enableDebugging() {
+        debugging = true;
     }
 
     public static void e(String text) {
@@ -38,9 +44,11 @@ public class Log {
     }
 
     public static void d(String text) {
-        synchronized (loggers) {
-            for (Logger logger : loggers) {
-                logger.d(text);
+        if (debugging) {
+            synchronized (loggers) {
+                for (Logger logger : loggers) {
+                    logger.d(text);
+                }
             }
         }
     }
