268a0a4ad64ad2b179f125d60a2706c509e09b95
Jan Lafferton <jan@lafferton.de>
2018-05-07 12:05:40 +0200

    Implemented basic hash generation algorithms

--- src/de/aeschool/hashgenerator/crypto/Hash.java
+++ src/de/aeschool/hashgenerator/crypto/Hash.java
@@ -1,5 +1,48 @@
 package de.aeschool.hashgenerator.crypto;
 
-public abstract class Hash {
-    public abstract String toString();
+import de.aeschool.hashgenerator.log.Log;
+
+import java.security.MessageDigest;
+
+public class Hash {
+    public static String md5(String text) {
+        return generateHash(text, "MD5");
+    }
+
+    public static String sha1(String text) {
+        return generateHash(text, "SHA-1");
+    }
+
+    public static String sha256(String text) {
+        return generateHash(text, "SHA-256");
+    }
+
+    private static String generateHash(String text, String hashType) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(hashType);
+            md.update(text.getBytes());
+
+            return toHexString(md.digest());
+        } catch (Exception e) {
+            Log.e(e);
+
+            return "An error occurred";
+        }
+    }
+
+    private static String toHexString(byte[] data) {
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < data.length; i++) {
+            String hex = Integer.toHexString(0xFF & data[i]);
+
+            if (hex.length() == 1) {
+                hexString.append("0");
+            }
+
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
 }
--- src/de/aeschool/hashgenerator/crypto/MD5Hash.java
+++
@@ -1,7 +0,0 @@
-package de.aeschool.hashgenerator.crypto;
-
-public class MD5Hash extends Hash {
-    public String toString() {
-        return "No yet implemented";
-    }
-}
